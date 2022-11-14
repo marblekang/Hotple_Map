0b02f4c9c833487b0b4ca7d370721e2e091211df
marblekang <kangbm930@gmail.com>
2022-11-14 15:18:11 +0900

    feat: msw 설정

--- hotple_statistics/src/main.tsx
+++ hotple_statistics/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { worker } from "./mocks/browser";
+if (process.env.NODE_ENV === "development") {
+  worker.start();
+}
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
